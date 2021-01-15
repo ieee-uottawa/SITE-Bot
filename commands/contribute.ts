1870f22acaf065f38a33f5208fff2d30809926ac
aMahanna <anthony.mahanna@gmail.com>
2021-01-14 23:34:24 -0500

    fix: contribute public command

--- commands/contribute.ts
+++ commands/contribute.ts
@@ -4,18 +4,32 @@ import { Command, CommandDefinition } from ".";
 export const description: CommandDefinition = {
   name: "Contribute",
   description: "Prints instructions for adding a command to the bot.",
-  usage: "!contribute",
+  usage: "!contribute \n\t!contribute public",
   key: "contribute",
 };
 
+/**
+ * Uses regex to check if an optional parameter was provided
+ * @param content The message provided by the user.
+ */
+function isCommandPublic(content: string): boolean {
+  const match = content.match(/\b(\w*public\w*)\b/g);
+  if (match === null) return false // Quit early if no matches found.
+  return true;
+}
+
 export const action = (message: Message) => {
-  message.author.send(
+  const isPublic = isCommandPublic(message.content);
+  const messenger = isPublic ? message.channel : message.author; 
+
+  messenger.send(
     "Contribute to the **IEEE uOttawa SITE-Bot**!" +
       "\n\nLook through the source code at" +
       " https://github.com/ieee-uottawa/SITE-Bot" +
       " and send a pull request."
   );
-  message.channel.send("Check your inbox!");
+  if (!isPublic) 
+    message.channel.send("I've sent you the details! Type `!contribute public` to print the info to this channel.")
 };
 
 export const command: Command = {
