acb68dffd910f3ac75551de39914dc32b5bcde75
aMahanna <anthony.mahanna@gmail.com>
2021-01-14 23:34:30 -0500

    fix: help public command

--- commands/help.ts
+++ commands/help.ts
@@ -5,13 +5,27 @@ import { help } from "../utils";
 export const description: CommandDefinition = {
   name: "Help",
   description: "Prints help text for included commands.",
-  usage: "!help",
+  usage: "!help \n\t!help public",
   key: "help",
 };
 
+/**
+ * Uses regex to check if an optional parameter was provided
+ * @param content The message provided by the user.
+ */
+function isCommandPublic(content: string): boolean {
+  const match = content.match(/\b(\w*public\w*)\b/g);
+  if (match === null) return false // Quit early if no matches found.
+  return true;
+}
+
 export const action = (message: Message) => {
-  message.author.send(help(commands));
-  message.channel.send("Check your inbox!");
+  const isPublic = isCommandPublic(message.content);
+  const messenger = isPublic ? message.channel : message.author; 
+
+  messenger.send(help(commands));
+  if (!isPublic) 
+    message.channel.send("I've sent you the user manual! Type `!help public` to print the manual to this channel.")
 };
 
 export const command: Command = {
