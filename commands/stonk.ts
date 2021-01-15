2e938711b33fa59024e01869320ecde815f39ac3
aMahanna <anthony.mahanna@gmail.com>
2021-01-15 15:25:11 -0500

    fix: cleanup stonk

--- commands/stonk.ts
+++ commands/stonk.ts
@@ -1,30 +1,40 @@
 import { Message } from "discord.js";
-import { Command, CommandDefinition } from ".";
+import { Command, CommandDefinition, Action} from ".";
 import dotenv from "dotenv";
+import fetch from "node-fetch";
 
 dotenv.config();
-const fetch = require("node-fetch"); // Requiring fetch to make the stock API calls
-const FMP_KEY = process.env.FMP_API_KEY; // This will need to be added in the .env file
+const FMP_KEY = process.env.FMP_API_KEY || undefined;
 
 export const description: CommandDefinition = {
   name: "Stonk Watcher",
   description: "Displays stonk info for the symbol provided",
-  usage: ["!stonk", "!stonk <symbol>"],
-  key: "stonk",
+  usage: ["!stonk <symbol>"],
+  keys: ["stonk"],
 };
-  
-export const action = (message: Message) => {
-    const symbol = message.content.replace("!stonk", "");
-    fetch(`https://financialmodelingprep.com/api/v3/quote/${symbol === "" ? "^IXIC" : symbol}?apikey=${FMP_KEY}`)
-    .then((data: any) => data.json())
-    .then((res: any) => {
-        let stockInfo;
-        if (res === undefined || res.length === 0 ) // TODO: Figure out what happens to `res` when no API calls remain 
-            stockInfo = "Error: Invalid Symbol, or No More Calls Remaining";
-        else
-            stockInfo = `${res[0].name}\nCurrent: $${res[0].price}\nChange: ${res[0].changesPercentage}%\nOpen: $${res[0].open}\nPrevious Close: $${res[0].previousClose}`
-        message.channel.send(stockInfo);
-    });
+
+export const action: Action = (message, key) => {
+  const symbol = message.content.replace("!stonk", "");
+  if (symbol === "") return; 
+
+  if (FMP_KEY === undefined) {
+    message.channel.send("API Key Not Configured");
+    return;
+  }
+
+  fetch(`https://financialmodelingprep.com/api/v3/quote/${symbol}?apikey=${FMP_KEY}`)
+  .then((data: any) => data.json())
+  .then((res: any) => {
+      let stockInfo;
+      if (res === undefined || res.length === 0 ) // TODO: Figure out what happens to `res` when no API calls remain 
+          stockInfo = "Error: Invalid Symbol, or No More Calls Remaining";
+      else
+          stockInfo = `${res[0].name}\nCurrent: $${res[0].price}\nChange: ${res[0].changesPercentage}%\nOpen: $${res[0].open}\nPrevious Close: $${res[0].previousClose}`
+      message.channel.send(stockInfo);
+  })
+  .catch((err: any) => {
+    
+  });
 };
 
 export const command: Command = {
