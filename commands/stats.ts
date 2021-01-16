03ee989f207ea9fe4ebd7cd1445cf8e4fa8f6e57
aMahanna <anthony.mahanna@gmail.com>
2021-01-16 09:42:24 -0500

    fix: addressing PR comments

--- commands/stats.ts
+++ commands/stats.ts
@@ -14,7 +14,7 @@ export const action = (message : Message, key : string) => {
     if (botCount !== undefined) botCount++; // Bot doesn't include itself in the botCount lol
 
     //Get role counts
-    let rollCounts : {rName:string; count:number}[] = [];
+    let rollCounts : string[] = [];
     const rolesJSON : any = message.guild?.roles.cache.toJSON(); // convert role Map Collection to JSON
     for (const key in rolesJSON) { // Iterate through each role object
       const roleData : any = rolesJSON[key];
@@ -24,18 +24,14 @@ export const action = (message : Message, key : string) => {
       // If the role is deleted, is @everyone, is a bot role, or has a count of 0, skip
       if (roleData.deleted || roleName === '@everyone' || roleName === 'YAGPDB.xyz' || roleCount === 0) 
         continue
-
-      rollCounts.push({
-        rName: roleName,
-        count: roleCount
-      }); 
+        
+      rollCounts.push(`${roleName} Members: \`${roleCount}\``);
     }
     
-    let data = `Total Members: ${totalMembers}\nBot Count: ${botCount}\n`;
-    for (var index in rollCounts) // Append all rollCounts to the final message to send
-      data += `${rollCounts[index].rName} Members: ${rollCounts[index].count}\n`
+    let data = `\nTotal Members:  \`${totalMembers}\`\nBot Count: \`${botCount}\`\n`;
+    data += rollCounts.join("\n");
 
-    message.author.send(data);
+    message.reply(data);
 };
 
 export const command: Command = {
